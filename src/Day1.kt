7eaa6fcaaee31b110dd05524eafb9b74f4033f13
ras <ras_t110e5@hotmail.com>
2022-12-01 09:08:47 -0300

    Solved first day

--- src/Day1.kt
+++ src/Day1.kt
@@ -9,12 +9,34 @@ fun main() {
 }
 
 class Day1(private val input: List<String>) {
-  fun part1(): Int {
-    return input.size
+  fun part1(): Int? {
+    return groupFoodByElf().maxOfOrNull { it.totalCalories() }
   }
 
   fun part2(): Int {
-    return input.size
+    return groupFoodByElf().map { it.totalCalories() }
+      .sortedDescending().subList(0, 3).sum()
+  }
+
+  private fun groupFoodByElf(): MutableList<Elf> {
+    var elf = Elf()
+    val elves = mutableListOf(elf)
+    for (line in input)
+      if (line.isNotBlank())
+        elf.addFood(line.toInt())
+      else {
+        elf = Elf()
+        elves.add(elf)
+      }
+    return elves
   }
 }
 
+class Elf(private val foods: MutableList<Int> = mutableListOf()) {
+
+  fun addFood(food: Int) {
+    this.foods.add(food)
+  }
+
+  fun totalCalories() = foods.sum()
+}
