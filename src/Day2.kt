3fe57fa4c8bd70333fe07d108b8bbcc7b86e3232
ras <ras_t110e5@hotmail.com>
2022-12-02 09:23:33 -0300

    Added solution for day 2

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,3 +1,5 @@
+import java.lang.IllegalArgumentException
+
 fun main() {
   val day = 2
   println(Day2(readTest(day)).part1())
@@ -10,11 +12,65 @@ fun main() {
 
 class Day2(private val input: List<String>) {
   fun part1(): Int {
-    return input.size
+    var score = 0
+    for (round in input) {
+      val selections = round.split(" ")
+      val opponentPlay = Selection.valueOf(selections[0])
+      val yourPlay = Selection.byStrategy(selections[1].single())
+      score += yourPlay.value
+      if (yourPlay.winsAgainst(opponentPlay)) score += 6
+      else if (isDraw(yourPlay, opponentPlay)) score += 3
+    }
+    return score
   }
 
+  private fun isDraw(yourPlay: Selection, opponentPlay: Selection) = yourPlay == opponentPlay
+
   fun part2(): Int {
-    return input.size
+    var score = 0
+    for (round in input) {
+      val selections = round.split(" ")
+      val opponentPlay = Selection.valueOf(selections[0])
+      when (selections[1].single()) {
+        'X' -> score += opponentPlay.beats().value
+        'Y' -> score += opponentPlay.value + 3
+        'Z' -> score += opponentPlay.losses().value + 6
+      }
+    }
+    return score
+  }
+}
+
+enum class Selection(val value: Int) {
+  A(1) {
+    override fun winsAgainst(opponentPlay: Selection): Boolean = opponentPlay == beats()
+    override fun beats(): Selection = C
+    override fun losses(): Selection = B
+  },
+  B(2) {
+    override fun winsAgainst(opponentPlay: Selection): Boolean = opponentPlay == beats()
+    override fun beats(): Selection = A
+    override fun losses(): Selection = C
+  },
+  C(3) {
+    override fun winsAgainst(opponentPlay: Selection): Boolean = opponentPlay == beats()
+    override fun beats(): Selection = B
+    override fun losses(): Selection = A
+  };
+
+  abstract fun winsAgainst(opponentPlay: Selection): Boolean
+  abstract fun beats(): Selection
+  abstract fun losses(): Selection
+
+  companion object {
+    fun byStrategy(strategySelection: Char): Selection {
+      return when (strategySelection) {
+        'X' -> A
+        'Y' -> B
+        'Z' -> C
+        else -> throw IllegalArgumentException()
+      }
+    }
   }
 }
 
